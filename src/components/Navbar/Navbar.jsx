81573b66f136aba6d11f6bdb11c30425e7d43c83
Athika Fatima <athikafatima16@gmail.com>
2023-10-22 20:32:36 -0400

    Made the code mobile responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,34 @@
-import { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import "./Navbar.css";
 
-const Navbar = () =>{
-
-
-
-    return (<>
-        <div className="navbar">
-            <div className="innerElements">
-                <Link to='/transactions'>
-                Transactions
-                </Link>
-                
-            </div>
-            <div className="innerElements">
-                <Link to='/addresses'>
-                Addresses
-                </Link>
-            </div>
-            <div className="innerElements">
-                <Link to='/transfer'>
-                Transfer
-                </Link>
-            </div>
-            <div className="innerElements">
-                <Link to='/wallet'>
-                Wallet
-                </Link>
-            </div>
-
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="innerElements">
+          <Link to="/transactions" className="nav-link">
+            Transactions
+          </Link>
         </div>
-    </>);
-}
+        <div className="innerElements">
+          <Link to="/addresses" className="nav-link">
+            Addresses
+          </Link>
+        </div>
+        <div className="innerElements">
+          <Link to="/transfer" className="nav-link">
+            Transfer
+          </Link>
+        </div>
+        <div className="innerElements">
+          <Link to="/wallet" className="nav-link">
+            Wallet
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
